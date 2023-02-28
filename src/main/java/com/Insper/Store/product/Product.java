f8ca8ee83ae75cb79fbf2caaab1ac75f2e92f751
WeeverAlex <awever4000@gmail.com>
2023-02-28 10:58:00 -0300

    store

---
+++ src/main/java/com/Insper/Store/product/Product.java
@@ -0,0 +1,18 @@
+package com.Insper.Store.product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class Product {
+    private String name;
+    private String stock;
+    private String price;
+
+
+
+
+
+
+}
